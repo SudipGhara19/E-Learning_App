d122a7a662a5f7afa7e0f0297cd48cbae51809cf
SudipGhara19 <sudipghara135@gmail.com>
2024-01-20 21:59:09 +0530

    added /courses:courseId route in the app.js to go to Details page, on courses.js added link to the card, on Details.js used -useParams- and add the course details by using individual course-id

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Nav from "./components/nav/Nav";
 import Courses from "./pages/app/courses/Courses";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import ErrorToast from "./components/error-toast/ErrorToast";
+import Details from "./pages/app/details/Details"
 
 function App() {
   const router = createBrowserRouter([
@@ -12,7 +13,12 @@ function App() {
       errorElement: <ErrorToast/>,
       children: [
         {index: true, element: <Hero/>},
-        {path: "/courses", element:<Courses/>},
+        {path: "/courses", 
+          children: [
+            {index: true, element: <Courses/>},
+            {path: ":courseId", element: <Details/>}
+          ],
+        },
       ],
     },
   ])
